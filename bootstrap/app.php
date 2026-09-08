c710a75808cb927798f72bc00e1ed23f65318b75
milcu11 <your-email@example.com>
2026-09-08 18:52:42 +0800

    Prevent logout CSRF expiration errors

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -22,6 +22,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->validateCsrfTokens(except: [
             'api/*',
             'api/webhooks/paymongo',
+            'logout',
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
